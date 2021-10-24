d96501cf09fa9f09c60e91583c21cea615a6cf53
Eduardo Santos <santos.eduardothomaz@gmail.com>
2021-10-24 01:55:30 -0300

    Fix(start errors) - server crashing solved

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
+
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-var indexRouter = require("./routes/index");
+import usersRouter from "./routes/users";
 
 var app = express();
 
@@ -13,6 +15,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
+app.use("/api/users", usersRouter);
 
 module.exports = app;

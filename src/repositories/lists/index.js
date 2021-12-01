c9efdd957fa8b641c6685575fd9471660a4f98df
Eduardo Santos <46734503+eduardothsantos@users.noreply.github.com>
2021-12-01 19:32:00 -0300

    fix: merge-conflicts (#16)

--- src/repositories/lists/index.js
+++ src/repositories/lists/index.js
@@ -52,7 +52,6 @@ export const NewListRepository = (database) => {
     if (list) return true;
     return false;
   };
-<<<<<<< HEAD
 
   const getBoard = async (listId) => {
     let list = await db.list.findUnique({ where: { id: listId } });
@@ -60,7 +59,4 @@ export const NewListRepository = (database) => {
     return;
   };
   return { create, update, deleteList, exists, getBoard };
-=======
-  return { create, update, deleteList, exists };
->>>>>>> main
 };
